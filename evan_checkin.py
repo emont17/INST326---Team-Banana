08858c13cd8b34b32102cc2a56c829b22159b208
emont17 <93267244+emont17@users.noreply.github.com>
2021-11-15 19:23:36 -0500

    Update evan_checkin.py

--- evan_checkin.py
+++ evan_checkin.py
@@ -8,24 +8,24 @@ class Mall:
     American_Eagle = 5
     QUIT = 6
 
-    def __init__(self, display):
-        self.display = display
-        
+    def menu(self):
         print('     MALL DIRECTORY')
         print('1) Nike')
         print('2) Dicks')
         print('3) Bass Pro Shops')
         print('4) Macys')
         print('5) American Eagle')
-        print('6) Leave Mall')
+        print('6) Leave Mall') 
         print()
+        
+        
     
 
-    def user(self):
+    def user_choice(self):
         """This function allows user to select a store to shop in"""
+        self.menu()
         choice = 0
-        while choice != QUIT:
-            self.display
+        while choice != "QUIT":
             choice = int(input('Which store would you like to shop at first? '))
             print()
             if choice == Nike:
@@ -43,9 +43,13 @@ class Mall:
             elif choice == American_Eagle:
                 print('You are now in American Eagle, shop away!')
                 print()
-            elif choice == QUIT:
+            elif choice == "QUIT":
                 print('You have left the Mall, have a great day!')
                 print()
             else:
                 print('ERROR: INVALID SELECTION')
                 print()
+
+    
+instance = Mall()
+instance.user_choice()
